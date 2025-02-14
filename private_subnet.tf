f460c280f9859332b94274a6bbe740dfb112830e
HJ <heejnam0918@gmail.com>
2025-02-15 02:33:01 +0900

    Update private_subnet.tf

---
+++ private_subnet.tf
@@ -0,0 +1,56 @@
+# Public Subnet 1
+resource "aws_subnet" "tf_pub_sub_1" {
+  vpc_id = aws_vpc.tf_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = ap-northeast-2a   # (Optional) AZ for the subnet
+  map_public_ip_on_launch = true   # 퍼블릭 IP 주소 자동 할당 : 활성화
+
+  tags = {
+    Name = "tf_pub_sub_1"
+  }
+}
+
+resource "aws_subnet" "tf_pub_sub_2" {
+  vpc_id = aws_vpc.tf_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2c"
+  map_public_ip_on_launch = true   # 퍼블릭 IP 주소 자동 할당 : 활성화
+
+  tags = {
+    Name = "tf_pub_sub_2"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "tf_igw" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  tags = {
+    Name = "tf_igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "tf_pub_rtb" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  tags = {
+    Name = "tf_pub_rtb"
+  }
+}
+
+resource "aws_route" "tf_pub_route_igw" {
+  route_table_id = aws_route_table.tf_pub_rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.tf_igw.id
+}
+
+resource "aws_route_table_association" "tf_pub_sub_1_association" {
+  subnet_id      = aws_subnet.tf_pub_sub_1.id
+  route_table_id = aws_route_table.tf_pub_rtb.id
+}
+
+resource "aws_route_table_association" "tf_pub_sub_2_association" {
+  subnet_id      = aws_subnet.tf_pub_sub_2.id
+  route_table_id = aws_route_table.tf_pub_rtb.id
+}
