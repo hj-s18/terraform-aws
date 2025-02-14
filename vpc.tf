eb28455be36c73b4976d63089fbbbeff4d919254
HJ <heejnam0918@gmail.com>
2025-02-14 16:52:14 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,5 @@
 resource "aws_vpc" "tf_vpc" {
-  cidr_block = 10.0.0.0/16   # (Optional) The IPv4 CIDR block for the VPC
-  instance_tenancy = "default"   # (Optional) A tenancy option for instances launched into the VPC (Defaults to default)
-  enable_dns_support = true   # (Optional) Whether or not the VPC has DNS support (Defaults to true)
-  enable_dns_hostnames = false   # (Optional) Whether or not the VPC has DNS hostname support (Defaults to false)
+  cidr_block = 10.0.0.0/16
 
   tags = {
     Name = "tf_vpc"
