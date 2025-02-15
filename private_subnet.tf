66356822ff45fb44e49d60a084fe53c338ef36b8
HJ <heejnam0918@gmail.com>
2025-02-15 20:34:01 +0900

    Update private_subnet.tf

--- private_subnet.tf
+++ private_subnet.tf
@@ -1,9 +1,9 @@
 # Subnet (Private)
 resource "aws_subnet" "tf_pri_sub_1" {
-  vpc_id                 = aws_vpc.tf_vpc.id  # (Required) The VPC ID.
-  cidr_block             = "10.0.3.0/24"  # (Optional) The IPv4 CIDR block for the subnet.
-  availability_zone      = "ap-northeast-2a"  # (Optional) AZ for the subnet.
-  map_public_ip_on_launch = false   # (Optional) Specify true to indicate that instances launched into the subnet should be assigned a public IP address. Default is false.
+  vpc_id                 = aws_vpc.tf_vpc.id         # (Required) The VPC ID.
+  cidr_block             = "10.0.3.0/24"             # (Optional) The IPv4 CIDR block for the subnet.
+  availability_zone      = "ap-northeast-2a"         # (Optional) AZ for the subnet.
+  map_public_ip_on_launch = false                    # (Optional) Specify true to indicate that instances launched into the subnet should be assigned a public IP address. Default is false.
 
   tags = {
     Name                 = "tf_pri_sub_1"
@@ -46,7 +46,7 @@ resource "aws_subnet" "tf_rds_sub_2" {
 
 # Single EIP associated with an instance
 resource "aws_eip" "tf_eip" {
-  domain = "vpc"  # (Optional) Indicates if this EIP is for use in VPC.
+  domain = "vpc"                                 # (Optional) Indicates if this EIP is for use in VPC.
   
   tags = {
     Name = "tf_eip"
@@ -55,8 +55,8 @@ resource "aws_eip" "tf_eip" {
 
 # Public NAT
 resource "aws_nat_gateway" "tf_nat" {
-  allocation_id = aws_eip.tf_eip.allocation_id  # (Optional) The Allocation ID of the Elastic IP address for the NAT Gateway. Required for connectivity_type of public.
-  subnet_id     = aws_subnet.tf_pub_sub_2.id  # (Required) The Subnet ID of the subnet in which to place the NAT Gateway.
+  allocation_id = aws_eip.tf_eip.allocation_id   # (Optional) The Allocation ID of the Elastic IP address for the NAT Gateway. Required for connectivity_type of public.
+  subnet_id     = aws_subnet.tf_pub_sub_2.id     # (Required) The Subnet ID of the subnet in which to place the NAT Gateway.
   depends_on = [aws_eip.tf_eip]
 
   tags = {
