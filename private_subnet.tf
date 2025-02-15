b946ee6c1e058fc03cad10bb9ab73220f3109164
HJ <heejnam0918@gmail.com>
2025-02-15 19:39:11 +0900

    Update private_subnet.tf

--- private_subnet.tf
+++ private_subnet.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "tf_pri_sub_1" {
   vpc_id                 = aws_vpc.tf_vpc.id
   cidr_block             = "10.0.3.0/24"
-  availability_zone      = "ap-northeast-2a
+  availability_zone      = "ap-northeast-2a"
 
   tags = {
     Name                 = "tf_pri_sub_1"
@@ -12,7 +12,7 @@ resource "aws_subnet" "tf_pri_sub_1" {
 resource "aws_subnet" "tf_pri_sub_2" {
   vpc_id                 = aws_vpc.tf_vpc.id
   cidr_block             = "10.0.4.0/24"
-  availability_zone      = "ap-northeast-2c
+  availability_zone      = "ap-northeast-2c"
 
   tags = {
     Name                 = "tf_pri_sub_2"
