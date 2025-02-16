1be01423a2142cb28aeac08746713fa6d6afb85e
HJ <heejnam0918@gmail.com>
2025-02-16 23:18:02 +0900

    Update ✏️vpc.tf

--- ✏️vpc.tf
+++ ✏️vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "tf_vpc" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = True
+  enable_dns_hostnames = True  # DNS 호스트 이름 활성화해야 VPC 내에서 Route53 CNAME 사용 가능
 
   tags = {
     Name = "tf_vpc"
