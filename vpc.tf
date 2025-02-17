b1b1e61345f335550451d0e965288af9e57dd92a
HJ <heejnam0918@gmail.com>
2025-02-17 11:34:36 +0900

    Update and rename ✏️vpc.tf to vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "tf_vpc" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true  # Route 53 Private Hosted Zone 사용 조건
+  enable_dns_hostnames = true
 
   tags = {
     Name = "tf_vpc"
