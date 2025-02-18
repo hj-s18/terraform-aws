f6f4f415e0ac4cd8108e6cb957be41d99ddf1686
HJ <heejnam0918@gmail.com>
2025-02-18 19:29:36 +0900

    Update ✏️eks_nodegroup.tf

--- ✏️eks_nodegroup.tf
+++ ✏️eks_nodegroup.tf
@@ -95,66 +95,76 @@ resource "aws_security_group" "tf_eks_node_group_sg" {
   description = "EKS Node Group Security Group"
   vpc_id      = aws_vpc.tf_vpc.id
 
-  # EKS 클러스터 → 노드 (Kubelet API 관리)
   ingress {
-    from_port       = 10250
-    to_port         = 10250
+    from_port       = 4443
+    to_port         = 4443
     protocol        = "tcp"
-    description     = "Allow cluster to manage worker nodes"  # "Allow EKS control plane to manage nodes" ?
+    description     = "Cluster API to node 4443/tcp webhook"
     security_groups = [aws_security_group.tf_eks_cluster_sg.id]
   }
 
-  # EKS 클러스터 → 노드 (Webhook, API 통신 : API Server)
+  ingress {
+    from_port       = 53
+    to_port         = 53
+    protocol        = "udp"
+    description     = "Node to node CoreDNS UDP"
+    self            = true
+  }
+
+  ingress {
+    from_port       = 53
+    to_port         = 53
+    protocol        = "tcp"
+    description     = "Node to node CoreDNS"
+    self            = true
+  }
+
   ingress {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
-    description     = "Allow cluster to communicate with worker nodes"
-    security_groups = [aws_security_group.tf_eks_cluster_sg.id]
-  }
-
-  # DNS 요청 허용 (CoreDNS)
-  ingress {
-    from_port       = 53
-    to_port         = 53
-    protocol        = "udp"
-    description     = "Allow worker nodes to use DNS (UDP)"
+    description     = "Cluster API to node groups"
     security_groups = [aws_security_group.tf_eks_cluster_sg.id]
   }
 
   ingress {
-    from_port       = 53
-    to_port         = 53
+    from_port       = 6443
+    to_port         = 6443
     protocol        = "tcp"
-    description     = "Allow worker nodes to use DNS (TCP)"
+    description     = "Cluster API to node 6443/tcp webhook"
     security_groups = [aws_security_group.tf_eks_cluster_sg.id]
   }
 
-  # 노드 간 통신 (컨테이너 간 트래픽)
   ingress {
-    from_port   = 1025
-    to_port     = 65535
-    protocol    = "tcp"
-    description = "Allow node-to-node communication"
-    self        = true
+    from_port       = 1025
+    to_port         = 65535
+    protocol        = "tcp"
+    description     = "Node to node ingress on ephemeral ports"
+    self            = true
   }
 
-  # 노드 → AWS API, ECR, S3 (필수)
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    description = "Allow nodes to communicate with AWS services"
-    cidr_blocks = ["0.0.0.0/0"]
+  ingress {
+    from_port       = 9443
+    to_port         = 9443
+    protocol        = "tcp"
+    description     = "Cluster API to node 9443/tcp webhook"
+    security_groups = [aws_security_group.tf_eks_cluster_sg.id]
   }
 
-  # 노드 → 외부 DNS 조회 (Amazon DNS or External DNS)
-  egress {
-    from_port   = 53
-    to_port     = 53
-    protocol    = "udp"
-    description = "Allow nodes to resolve external DNS"
-    cidr_blocks = ["0.0.0.0/0"]
+  ingress {
+    from_port       = 10250
+    to_port         = 10250
+    protocol        = "tcp"
+    description     = "Cluster API to node kubelets"
+    security_groups = [aws_security_group.tf_eks_cluster_sg.id]
+  }
+
+  ingress {
+    from_port       = 8443
+    to_port         = 8443
+    protocol        = "tcp"
+    description     = "Cluster API to node 8443/tcp webhook"
+    security_groups = [aws_security_group.tf_eks_cluster_sg.id]
   }
 
   tags = {
